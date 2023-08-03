bf9a3bbc40c3c4ad5c0c6838bfd2d42333656b5a
prastowoadi18 <prastowoadiworkv@gmail.com>
2023-08-03 10:10:23 +0700

    inital

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+const webpack = (config) => {
+  config.module.rules.push({
+    test: /\.node/,
+    use: "raw-loader",
+  });
+
+  return config;
 };
 
-module.exports = nextConfig;
+module.exports = {
+  webpack,
+  reactStrictMode: true,
+};
